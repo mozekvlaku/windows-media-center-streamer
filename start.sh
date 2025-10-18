2a022e10646aa3512b2aa247da16f2e665cff895
namulnae <thomas@maeum.one>
2025-10-19 00:53:05 +0200

    uzgsdf

--- start.sh
+++ start.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -e
 
-# spust X server
-Xvfb :1 -screen 0 1280x720x24 &
-export DISPLAY=:1
+# --- ffmpeg grab přímo z VNC ---
+# uprav IP a port podle tvého Windows XP VM
+VNC_HOST="192.168.102.142"
+VNC_PORT="5000"
 
-# spust Chromium na noVNC URL
-chromium-browser --no-sandbox --disable-gpu --kiosk "$NOVNC_URL" &
+# vytvoř HLS výstupní adresář
+mkdir -p /var/www/llhls
 
-sleep 5
-
-# spust ffmpeg grab z X displayu → HLS
+# ffmpeg: VNC -> HLS
 ffmpeg -hide_banner -loglevel info \
-  -f x11grab -r 30 -s 1280x720 -i :1.0 \
+  -f vnc -r 30 -i ${VNC_HOST}:${VNC_PORT} \
   -c:v libx264 -preset ultrafast -tune zerolatency -g 60 -keyint_min 30 \
   -sc_threshold 0 -b:v 2500k -maxrate 3000k -bufsize 5000k \
   -c:a aac -b:a 96k \
